01810ca24c20a0b51f3a502127159119fe4598cb
Jay Medina <jay.medina.celis@gmail.com>
2023-05-11 14:25:22 -0400

    Declares all consturctors and defines Card object

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+(function () {
+  // constructor for Game, Player, Deck, and Card
+  function Card({ value = 0, suite = "" } = {}) {
+    this.value = value;
+    this.suite = suite;
+  }
+  function Deck() {}
+  function Player() {}
+  function Game() {}
+})();
