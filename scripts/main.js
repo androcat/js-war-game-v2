73e1ab7d4f9b5fe1221f873b738578e39c4642e2
Jay Medina <jay.medina.celis@gmail.com>
2023-05-11 21:38:30 -0400

    Defines buildDeck for Deck objects

--- scripts/main.js
+++ scripts/main.js
@@ -13,36 +13,60 @@
     "8",
     "9",
     "10",
-    "J",
-    "Q",
-    "K",
+    "Jack",
+    "Queen",
+    "King",
   ];
 
   const cardSuites = ["Diamonds", "Hearts", "Spades", "Clubs"];
 
   // constructor for Game, Player, Deck, and Card
-  function Card({ value = 0, suite = "" } = {}) {
+  function Card({ value = "", suite = "" } = {}) {
     this.value = value;
     this.suite = suite;
   }
-  function Deck({ deckOfCards = [] } = {}) {
-    this.deckOfCards = deckOfCards;
-  }
   // Returns an array of strings ex: 5 of Hearts
-  Card.prototype.buildDeck = function (arrValues, arrSuites) {
-    let deck = [];
+
+  function Deck({ cards = [] } = {}) {
+    this.cards = cards; // needs to have 52 card objs
+  }
+  Deck.prototype.buildDeck = function (arrValues, arrSuites) {
+    // let deck = Array(arrValues.length * arrSuites.length);
+
+    // Why does this give only the first letter ???
+    // A: it was bc i had it like: [val] and [suite]
     for (const val of arrValues) {
       for (const suite of arrSuites) {
-        deck.push(`${val} of ${suite}`);
+        this.cards.push(new Card({ value: val, suite: suite }));
       }
     }
-    return deck;
+    // this works too
+    // for (let i = 0; i < arrSuites.length; i++) {
+    //   for (let j = 0; j < arrValues.length; j++) {
+    //     this.cards.push(new Card({ value: arrValues[j], suite: arrSuites[i] }));
+    //   }
+    // }
+
+    return this.cards;
   };
 
-  let testCard = new Card();
-  const deckArr = testCard.buildDeck(cardValues, cardSuites);
-  let testDeck = new Deck();
-  console.table(deckArr);
+  //   const testCard = new Card();
+  const testDeck = new Deck();
+
+  console.log(testDeck.buildDeck(cardValues, cardSuites));
+
+  // Using sort
+  Deck.prototype.shuffle = function (arr) {
+    arr.deckOfCards = arr.sort(() => Math.random() - 0.5);
+  };
+
+  //   let testCard = new Card();
+  //   const deckArr = testCard.buildDeck(cardValues, cardSuites);
+  //   console.table(deckArr);
+
+  //   let testDeck = new Deck(deckArr);
+  //   let testShuffleDeck = testDeck.shuffle(testDeck.deckOfCards);
+  //   console.log(testShuffleDeck);
 
   function Player({ cards = [] } = {}) {
     this.cards = cards;
