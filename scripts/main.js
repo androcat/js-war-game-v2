1783f0ebab456612a583a8940d23002f3c60b539
Jay Medina <jay.medina.celis@gmail.com>
2023-06-13 15:21:16 -0400

    Working Logic- huzzah

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
 (function () {
   "use strict";
 
+  const cpuCardText = document.getElementById("computer-card-text");
+  const cpuCardHand = document.getElementById("computer-hand");
+
+  const playerCardText = document.getElementById("player-card-text");
+  const playerCardHand = document.getElementById("player-hand");
+
+  const pullCardBtn = document.getElementById("pull-card-btn");
+
   // Array of card values and suites
   const cardValues = [
-    "A",
     "2",
     "3",
     "4",
@@ -16,16 +23,20 @@
     "Jack",
     "Queen",
     "King",
+    "Ace",
   ];
 
-  const cardSuites = ["Diamonds", "Hearts", "Spades", "Clubs"];
+  const cardSuites = ["♣", "♦", "♥", "♠"];
 
   // constructor for Game, Player, Deck, and Card
   function Card({ value = "", suite = "" } = {}) {
     this.value = value;
     this.suite = suite;
   }
-  // Returns an array of strings ex: 5 of Hearts
+
+  Card.prototype.print = function () {
+    return `${this.value} of ${this.suite}`;
+  };
 
   function Deck() {
     this.cards = []; // needs to have 52 card objs
@@ -36,53 +47,36 @@
       }
     }
   }
-  //   Deck.prototype.buildDeck = function (arrValues, arrSuites) {
-  //     // let deck = Array(arrValues.length * arrSuites.length);
-
-  //     // Why does this give only the first letter ???
-  //     // A: it was bc i had it like: [val] and [suite]
-  //     for (const val of arrValues) {
-  //       for (const suite of arrSuites) {
-  //         this.cards.push(new Card({ value: val, suite: suite }));
-  //       }
-  //     }
-  //     // this works too
-  //     // for (let i = 0; i < arrSuites.length; i++) {
-  //     //   for (let j = 0; j < arrValues.length; j++) {
-  //     //     this.cards.push(new Card({ value: arrValues[j], suite: arrSuites[i] }));
-  //     //   }
-  //     // }
-
-  //     return this.cards;
-  //   };
 
   // Using sort
   Deck.prototype.shuffle = function () {
     return this.cards.sort(() => Math.random() - 0.5);
   };
-  //   const copyShuffledCards = testDeck.shuffle(testDeck.cards);
-  //   console.log("Copy of shuffled deck", copyShuffledCards);
-  //   console.log("Test deck", testDeck);
 
-  // change from Deck obj to [] because we only want half of the array of cards deck has after it has been shuffled
   function Player() {
     this.hand = [];
   }
+
+  Player.prototype.shuffle = function () {
+    return this.hand.sort(() => Math.random() - 0.5);
+  };
+
   // Successfully pulls top card
   /// put on game instead bc it will be keeping track of the cards vvv
   Player.prototype.pullCard = function (pulledCardArr) {
     // console.log(pulledCardArr);
     // console.log(this.hand); // works as expected, can delete
 
-    pulledCardArr.push(this.hand.shift()); // or unshift() ...
-
-    return pulledCardArr[pulledCardArr.length - 1]; // and [0]
+    pulledCardArr.unshift(this.hand.shift()); // shift() removes 1st card and gives it onto the 'top'/start of pulledCardArr
+    //IF A HAND IS EMPTY AND WE ARE IN THE LAST PART OF THE GAME, COULD BE UNSHIFTING IN UNDEFINED
+    return pulledCardArr[0]; //gives back card just added
   };
 
   function Game() {
     this.player1 = new Player();
     this.player2 = new Player();
     this.deck = new Deck();
+    this.pulledCardArr = [];
   }
   // will pass in 'deck' an array of card objs, preferably pre-shuffled
   Game.prototype.deal = function () {
@@ -95,67 +89,95 @@
     }
   };
 
-  Game.prototype.isMatch = function (card1, card2) {
-    while (card1.value === card2.value) {
-      console.log("is firing");
-      for (let i = 0; i < 3; i++) {
-        setTimeout(this.player1.pullCard, 3000);
-        setTimeout(this.player2.pullCard, 3000);
-      }
-      if (card1.value !== card2.value) {
-        break;
-      }
-    }
+  Game.prototype.draw = function () {
+    let cardCPU = this.player1.pullCard(this.pulledCardArr);
+    let cardPlayer = this.player2.pullCard(this.pulledCardArr);
+
+    console.table("CPU pulled", cardCPU);
+    console.table("Player pulled", cardPlayer);
+    cpuCardText.innerText = cardCPU.print();
+    playerCardText.innerText = cardPlayer.print();
+
+    console.table("CPU hand", CPU.hand);
+    console.table("Player hand", player.hand);
+    cpuCardHand.innerText = this.player1.hand.length;
+    playerCardHand.innerText = this.player2.hand.length;
+
+    // this.pulledCardArr = [cardCPU, cardPlayer, ...this.pulledCardArr];
+    this.isMatch(cardCPU, cardPlayer);
   };
 
-  Game.prototype.findWinner = function (player1, player2) {
-    let winner = false;
-    if (player1.hand.length === 0) {
-      winner = player2;
-      alert("You win !");
-    } else {
-      winner = player1;
-      alert("CPU wins");
+  Game.prototype.isMatch = function (card1, card2) {
+    console.log("");
+    if (card1.value === card2.value) {
+      // REFACTOR
+      cpuCardText.innerText = "WAR!";
+      playerCardText.innerText = "WAR!";
+      this.pulledCardArr = [
+        ...this.player1.hand.splice(0, 3), // Grabbing the first 3 of the players' hands to add to pot
+        ...this.player2.hand.splice(0, 3),
+        ...this.pulledCardArr,
+      ];
+      console.log("Pot after war", this.pulledCardArr);
+      setTimeout(() => {
+        cpuCardText.innerText = "Shuffle";
+        playerCardText.innerText = "Shuffle";
+        this.player1.shuffle();
+        this.player2.shuffle();
+      }, 1000);
+    } else if (
+      cardValues.indexOf(card1.value) > cardValues.indexOf(card2.value)
+    ) {
+      console.log("CPU wins Round");
+      console.log("Pot", this.pulledCardArr);
+      console.log("CPU hand", this.player1.hand);
+      console.log("Player hand", this.player2.hand);
+      this.player1.hand = [...this.player1.hand, ...this.pulledCardArr];
+      this.pulledCardArr = [];
+      console.table("Player 1 should have the pot now", this.player1.hand);
+    } else if (
+      cardValues.indexOf(card1.value) < cardValues.indexOf(card2.value)
+    ) {
+      console.log("Player wins Round");
+      console.log("Pot", this.pulledCardArr);
+      console.log("CPU hand", this.player1.hand);
+      console.log("Player hand", this.player2.hand);
+      this.player2.hand = [...this.player2.hand, ...this.pulledCardArr];
+      this.pulledCardArr = [];
+      console.table("Player 2 should have the pot now", this.player2.hand);
     }
-    return winner;
+
+    if (this.player1.hand.length === 0) {
+      alert("You win !");
+    } else if (this.player2.hand.length === 0) {
+      alert("CPU wins!");
+    }
+    cpuCardHand.innerText = this.player1.hand.length;
+    playerCardHand.innerText = this.player2.hand.length;
+  };
+
+  Game.prototype.play = function () {
+    let cardCPU = CPU.pullCard(pulledCards);
+    let cardPlayer = player.pullCard(pulledCards);
+
+    testGame.isMatch(cardCPU, cardPlayer);
   };
 
   let pulledCards = [];
 
-  const testGame = new Game(); // creates a game with 2 players who have empty hands- let's fix that !
-  //   console.log(testGame.player1, testGame.player2);
-  testGame.deck.shuffle(); //UNCOMMENT IN LOGIC
+  const testGame = new Game();
+  testGame.deck.shuffle();
   testGame.deal();
   console.log(testGame.player1, testGame.player2);
 
-  // THE REAL MAIN.JS CONTENT:
-  // Sets up players and foundation for choosing players in future iterations of the game
   const CPU = testGame.player1;
   const player = testGame.player2;
-  // While neither player has 0 cards, keep playing
-  while (CPU.hand.length > 0 || player.hand.length > 0) {
-    let cardCPU = CPU.pullCard(pulledCards);
-    let cardPlayer = player.pullCard(pulledCards);
 
-    console.table("CPU pulled", cardCPU);
-    console.table("Player pulled", cardPlayer);
+  let i = 50;
+  // while (CPU.hand.length > 0 && player.hand.length > 0 && i) {
+  //   testGame.draw();
+  //   i--;
+  // }
 
-    console.table("CPU hand", CPU.hand);
-    console.table("Player hand", player.hand);
-
-    testGame.isMatch(cardCPU, cardPlayer);
-
-    if (testGame.findWinner(CPU, player)) {
-      console.log("this fires");
-      break;
-    }
-  }
-
-  //   console.log("CPU pulled", cardCPU);
-  //   console.log("Player pulled", cardPlayer);
-
-  // console.log("should return a Card", testPlayer.pullCard(pulledCards));
-  // console.log("Should return an array with 1 card", pulledCards);
-
-  //   const testGame = Game();
+  pullCardBtn.addEventListener("click", () => testGame.draw());
 })();
